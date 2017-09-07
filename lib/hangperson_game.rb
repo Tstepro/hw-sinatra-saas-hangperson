4be705fbbb1dae8f2d76a58fad43b8299f90e387
Timothy Stepro <timothystepro@gmail.com>
2017-09-07 20:55:19 +0000

    Ready for Heroku

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,11 +7,76 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  attr_accessor :word
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @num_guesses = 0
+  end
+  
+  def lowercase? x
+     x != x.downcase
+  end
+  
+  def letter? x
+    /[a-z]/i =~ x
+  end
+  
+  def valid? x
+    if x == nil || x.empty? || !letter?(x)
+      raise ArgumentError
+    end
+  end
+  
+  def guess x
+    valid?(x)
+    
+    if lowercase?(x)
+      return false  
+    end
+    
+    if @word.include? x
+      if !@guesses.include? x
+        @guesses += x.to_s
+        @num_guesses += 1
+        return true
+      end
+      return false
+    else
+      if !@wrong_guesses.include? x
+        @wrong_guesses += x.to_s
+        @num_guesses += 1
+        return true
+      end
+      return false
+    end
   end
 
+  def word_with_guesses
+    result = ''
+    @word.each_char do |x|
+      if guesses.include?(x)
+        result += x
+      else
+        result += '-'
+      end
+    end
+    result
+  end
+  
+  def check_win_or_lose
+    if @word.chars.uniq.sort == @guesses.chars.sort
+      return :win
+    elsif @num_guesses < 7
+      return :play
+    else
+      return :lose
+    end
+  end
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
